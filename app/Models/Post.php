16c621352a87cac831e6a9e2544d3c69adc45133
ApsaraGC <153732483+ApsaraGC@users.noreply.github.com>
2024-09-20 14:52:21 +0545

    Swagger Doc

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,55 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+
+/**
+ * @OA\Schema(
+ *     schema="Post",
+ *     type="object",
+ *     title="Post",
+ *     description="A Post model",
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="ID of the post",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="title",
+ *         type="string",
+ *         description="Title of the post",
+ *         example="My first post"
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         type="string",
+ *         description="Description of the post",
+ *         example="This is the description of the post"
+ *     ),
+ *     @OA\Property(
+ *         property="image",
+ *         type="string",
+ *         description="Image filename",
+ *         example="image.jpg"
+ *     ),
+ *     @OA\Property(
+ *         property="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Creation timestamp",
+ *         example="2024-09-19T14:28:23Z"
+ *     ),
+ *     @OA\Property(
+ *         property="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Last update timestamp",
+ *         example="2024-09-19T14:28:23Z"
+ *     )
+ * )
+ */
+
 class Post extends Model
 {
     use HasFactory;
--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('signup',[AuthController::class,'signup']);
 Route::post('login',[AuthController::class,'login']);
-
+/**
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     securityScheme="bearerAuth"
+ * )
+ */
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('logout',[AuthController::class,'logout']);
 
